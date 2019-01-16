2ed7ea9da61f9d417d2d262b5c96a00a402f512d
Inessa Borodich <iborodich@gds000387-nb.carina.griddynamics.net>
2019-01-17 00:12:22 +0400

    Add db

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path');
+let db = require('./db');
 
 const PORT = 3000;
 
 let app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(PORT, function() {
-    console.log('JSON Server is running on port ' + PORT);
-});
+app.get('/', (req, res) => res.send('Hello world'));
+app.get('/cities', (req, res) => res.send(db.cities));
+app.get('/actions', (req, res) => res.send(db.actions));
+app.get('/flights', (req, res) => res.send(db.flights));
 
+app.listen(PORT, () => console.log('\nJSON Server is running on port ' + PORT));
