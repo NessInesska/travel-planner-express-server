fea8b22d3002e3a1218482cd23a969c292b50b3b
Inessa Borodich <iborodich@gds000387-nb.carina.griddynamics.net>
2019-01-17 19:16:59 +0400

    Add CORS

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let cors = require('cors');
+
 let path = require('path');
 let db = require('./db');
 
-const PORT = 3000;
-
 let app = express();
+
+const PORT = 3000;
+const API_PREFIX = '/api';
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors);
 
-app.get('/', (req, res) => res.send('Hello world'));
-app.get('/cities', (req, res) => res.send(db.cities));
-app.get('/actions', (req, res) => res.send(db.actions));
-app.get('/flights', (req, res) => res.send(db.flights));
+app.use('/', (req, res) => res.send('Hello world'));
+app.use(`${API_PREFIX}/cities`, (req, res) => res.send(db.cities));
+app.get(`${API_PREFIX}/actions`, (req, res) => res.send(db.actions));
+app.get(`${API_PREFIX}/flights`, (req, res) => res.send(db.flights));
 
 app.listen(PORT, () => console.log('\nJSON Server is running on port ' + PORT));
