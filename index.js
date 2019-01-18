25dc193fc492ba0bd48150ee2d864349626c1a25
Inessa Borodich <iborodich@gds000387-nb.carina.griddynamics.net>
2019-01-18 18:46:14 +0400

    Fix cors

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let path = require('path');
 let cors = require('cors');
 
-let path = require('path');
+let app = express();
 let db = require('./db');
 
-let app = express();
-
 const PORT = 3000;
-const API_PREFIX = '/api';
+// const router = express.Router('src/db.json');
 
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors);
 
-app.use('/', (req, res) => res.send('Hello world'));
-app.use(`${API_PREFIX}/cities`, (req, res) => res.send(db.cities));
-app.get(`${API_PREFIX}/actions`, (req, res) => res.send(db.actions));
-app.get(`${API_PREFIX}/flights`, (req, res) => res.send(db.flights));
+const API_PREFIX = '/api';
+
+app.get('/', function (req, res) {
+    res.send('Hello ')
+});
+
+// app.use('/api', router);
+
+
+app.get(`${API_PREFIX}/cities`, function (req, res) {
+    res.send(db.cities)
+});
+
+app.get(`${API_PREFIX}/actions`, function (req, res) {
+    res.send(db.actions)
+});
+
+app.get(`${API_PREFIX}/flights`, function (req, res) {
+    res.send(db.flights)
+});
 
 app.listen(PORT, () => console.log('\nJSON Server is running on port ' + PORT));
